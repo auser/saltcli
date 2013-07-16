6d1340615f4b909383a4b1b209cc8aa5e5f80bf0
Ari Lerner <arilerner@mac.com>
2013-07-16 15:25:11 -0700

    Updated az

--- saltcli/commands/list.py
+++ saltcli/commands/list.py
@@ -15,11 +15,13 @@ class List(Command):
         print """{name}:
     id: {id}
     ip_address: {ip}
-    placement: {placement}
+    region: {placement}
+    availability zone: {az}
     environment: {environment}""".format( id=inst.id, 
                     ip=inst.ip_address, 
                     name=inst.tags.get('instance_name', None), 
                     placement=inst_d['region_name'],
+                    az=inst.placement,
                     environment=inst.tags.get('environment', None))
     else:
       colors = get_colors()
