5f775602ca7798c4c8c8e4b06397bd2d7c25df22
Ari Lerner <arilerner@mac.com>
2013-08-09 16:35:45 -0700

    Start ephemeral nodes block_mapping at c, rather than b (23 vs. 24)

--- saltcli/providers/aws/block_mappings.py
+++ saltcli/providers/aws/block_mappings.py
@@ -4,9 +4,9 @@ from boto.ec2.blockdevicemapping import BlockDeviceMapping
 def block_mappings(flavor):
   mapping = BlockDeviceMapping()
   if flavor == "hs1.8xlarge":
-    for i in range(0, 24):
+    for i in range(0, 23):
       eph = BlockDeviceType()
       eph.ephemeral_name = "ephemeral%d" % i
-      device = "/dev/sd%c" % chr(ord('b') + i)
+      device = "/dev/sd%c" % chr(ord('c') + i)
       mapping[device] = eph
   return mapping
