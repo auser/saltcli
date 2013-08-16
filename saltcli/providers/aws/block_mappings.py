e4e0751ecc11ee9fc99cd660629c0a4c05ace822
Brady Catherman <brady@orchestrate.io>
2013-08-16 10:55:20 -0700

    Disk layout improvements for hs1.8xlarge
    This makes sure that all 24 disks are setup, forces partitioning from
    amazon and generally make the layout work better.

--- saltcli/providers/aws/block_mappings.py
+++ saltcli/providers/aws/block_mappings.py
@@ -4,9 +4,9 @@ from boto.ec2.blockdevicemapping import BlockDeviceMapping
 def block_mappings(flavor):
   mapping = BlockDeviceMapping()
   if flavor == "hs1.8xlarge":
-    for i in range(0, 23):
+    for i in range(0, 24):
       eph = BlockDeviceType()
       eph.ephemeral_name = "ephemeral%d" % i
-      device = "/dev/sd%c" % chr(ord('c') + i)
+      device = "/dev/sd%c1" % chr(ord('b') + i)
       mapping[device] = eph
   return mapping
